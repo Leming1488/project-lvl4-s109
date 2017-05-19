009ae9159aa9b3608f3ba4aee92c4f6ee30c2725
Козельский Павел Сергеевич <pkozelskiy@n-core.ru>
2017-05-19 18:49:10 +0300

    [point-1] add routes

--- src/controllers/users.js
+++ src/controllers/users.js
@@ -6,9 +6,23 @@ export default (router, { User }) => {
       const users = await User.findAll();
       ctx.render('users', { users });
     })
-    .get('newUser', '/users/new', (ctx) => {
-      const user = User.build();
+    .get('newUser', '/users/new', async (ctx) => {
+      const { email } = ctx.request.body.form;
+      const user = await User.findOne({
+        where: {
+          email,
+        },
+      });
       ctx.render('users/new', { f: buildFormObj(user) });
+    })
+    .get('editUser', '/users/edit/:id', async (ctx) => {
+      const user = await User.findById(this.params);
+      ctx.render('users/edit', { f: buildFormObj(user) });
+    })
+    .delete('users', '/users', async (ctx) => {
+      const user = await User.findById(this.params)
+      await User.destroy({ where: {id: this.params}, force: true });
+      ctx.redirect(router.url('users'));
     })
     .post('users', '/users', async (ctx) => {
       const form = ctx.request.body.form;
